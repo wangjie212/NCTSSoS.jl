181a03e2d82b55d65e320768c82e6f2e1a79b284
exaclior <yushengzhao2020@outlook.com>
2025-03-26 20:47:08 +0800

    impl & test StatePolynomial

--- src/statepolynomial.jl
+++ src/statepolynomial.jl
@@ -1,15 +1,20 @@
-# TODO: update to conform with DynamicPolynomial v0.5.0 and up
 # T: type of coefficient
-# V: whether the variabels is commutative
-# M: ordering of the monomials Lexico or Reverse (this occurs in v0.5.0 of DynamicPolynomials)
-struct StatePolynomial{V,T} <: AbstractPolynomial{T}
+# V: whether the variabels is NonCommutative{CreationOrder} or Commutative{CreationOrder}
+# M: ordering of the monomials Graded{LexOrder} or else
+# FIXME: inheriting from AbstractPolynomial gives StackOverflowError when trying to print in REPL
+struct StatePolynomial{V,M,T} 
     coeffs::Vector{T}
-    formal_words::Vector{MonomialVector{V}} # expectation value of operators w.r.t general state
-    words::MonomialVector{V}
+    formal_words::Vector{Vector{Monomial{V,M}}} # expectation value of operators w.r.t general state
+    words::Vector{Monomial{V,M}}
 end
 
 # TODO: get a more user friendly way of declaring StatePolynomial
-function StatePolynomial{V,T}(coeffs::Vector{T}, formal_words::Vector{Vector{Monomial{V}}}, words::Vector{Monomial{V}}) where {V,T}
+function StatePolynomial(coeffs::Vector{T}, formal_words::Vector{Vector{Monomial{V,M}}}, words::Vector{Monomial{V,M}}) where {V,M,T}
     @assert length(coeffs) == length(formal_words) == length(words) "Coefficients, formal words, and words must have the same length"
-    StatePolynomial{V,T}(coeffs, formal_words, words)
+    StatePolynomial{V,M,T}(coeffs, formal_words, words)
 end
+
+Base.show(io::IO, sp::StatePolynomial) = print(io, join(map(zip(sp.coeffs,sp.formal_words,sp.words)) do (coeff, formal_word, word)
+    formal_word_str = join(map(x->"⟨$(x)⟩",string.(formal_word)), " ⋅ ")
+   "$(coeff) ⋅ $(formal_word_str) ⋅ $(word)" 
+end, " + "))
--- test/statepolynomial.jl
+++ test/statepolynomial.jl
@@ -1,9 +1,20 @@
 using Test, NCTSSoS
+using DynamicPolynomials
 
 @testset "State Polynomial" begin
     @ncpolyvar x[1:2]
-    @testset "Example 7.2.1" begin
+    @testset "free state polynomial" begin
         coeffs = [1.0, 2.0, 3.0]
-        formal_words = [[x[1]*x[2],x[2]*x[3]],[x[1]*x[2],x[2]*x[3]],[x[1]*x[2],x[2]*x[3]]]
+        formal_words = [[x[1]*x[2],x[2]^2],[x[1]*x[2]],[one(x[1])]]
+        words = [one(x[1]), one(x[2]), one(x[1])]
+        sp = StatePolynomial(coeffs, formal_words, words)
+        @test string(sp) == "1.0 ⋅ ⟨x[1]*x[2]⟩ ⋅ ⟨x[2]^2⟩ ⋅ 1 + 2.0 ⋅ ⟨x[1]*x[2]⟩ ⋅ 1 + 3.0 ⋅ ⟨1⟩ ⋅ 1"
+    end
+    @testset "nc state polynomial" begin
+        coeffs = [1.0, 2.0, 3.0]
+        formal_words = [[x[1]*x[2],x[2]^2],[x[1]*x[2]],[one(x[1])]]
+        words = [x[1], x[2]*x[2], one(x[1])]
+        sp = StatePolynomial(coeffs, formal_words, words)
+        @test string(sp) == "1.0 ⋅ ⟨x[1]*x[2]⟩ ⋅ ⟨x[2]^2⟩ ⋅ x[1] + 2.0 ⋅ ⟨x[1]*x[2]⟩ ⋅ x[2]^2 + 3.0 ⋅ ⟨1⟩ ⋅ 1"
     end
 end
