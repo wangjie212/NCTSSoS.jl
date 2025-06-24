ca82780005653de84654be3f193c2145266bacb9
exaclior <yushengzhao2020@outlook.com>
2025-06-24 10:59:30 +0800

    fix docs, use DocumenterCitations.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,8 @@
 using Documenter
 using NCTSSoS
+using DocumenterCitations
+
+bib = CitationBibliography(joinpath(@__DIR__, "src", "refs.bib"); style=:authoryear)
 
 makedocs(;
     sitename="NCTSSoS.jl",
@@ -7,11 +10,12 @@ makedocs(;
         "Home" => "index.md",
         "Noncommutative Polynomial Optimization" => "ncpop.md",
         "API" => "api.md",
-        "Examples" => ["Bell inequalities" => "bell.md", "Broyden Banded Function" => "broyden.md"],
-        "Performance" => ["FastPolynomials" => "performance.md"]
+        "Examples" => ["Bell inequalities" => "bell.md", "Broyden Banded Function" => "broyden.md", "Certifying Ground State" => "cert_ground_state.md"],
+        "References" => "reference.md"
     ],
+    plugins=[bib],
     modules=[NCTSSoS, NCTSSoS.FastPolynomials],
-    format=Documenter.HTML(; prettyurls=get(ENV, "CI", nothing) == "true"),
+    format=Documenter.HTML(; prettyurls=get(ENV, "CI", nothing) == "true", assets=String["assets/citations.css"]),
 )
 
-deploydocs(; repo="github.com/wangjie212/NCTSSoS.git")
+deploydocs(; repo="github.com/wangjie212/NCTSSoS.git", devbranch="main")
