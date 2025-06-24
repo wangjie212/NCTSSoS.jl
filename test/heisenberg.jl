ebed5339a21743fce5af6f4ea3e59d311e930ce8
exaclior <yushengzhao2020@outlook.com>
2025-06-24 15:17:10 +0800

    add failing complex coeffcient example

--- test/heisenberg.jl
+++ test/heisenberg.jl
@@ -4,9 +4,13 @@ using MosekTools
 N = 10
 @ncpolyvar x[1:N] y[1:N] z[1:N]
 
-ham = sum(1 / 4 * op[i] * op[mod1(i + 1, N)] for op in [x, y, z] for i in 1:N)
+ham = sum(ComplexF64(1 / 4) * op[i] * op[mod1(i + 1, N)] for op in [x, y, z] for i in 1:N)
 
-pop = PolyOpt(ham; comm_gps=[[x[i], y[i], z[i]] for i in 1:N], is_unipotent=true)
+eq_cons = reduce(vcat, [[x[i] * y[i] - im * z[i], y[i] * x[i] + im * z[i], y[i] * z[i] - im * x[i], z[i] * y[i] + im * x[i], z[i] * x[i] - im * y[i], x[i] * z[i] + im * y[i]] for i in 1:N])
+
+
+
+pop = PolyOpt(ham; eq_constraints=eq_cons, comm_gps=[[x[i], y[i], z[i]] for i in 1:N], is_unipotent=true)
 
 solver_config = SolverConfig(optimizer=Mosek.Optimizer)
 
