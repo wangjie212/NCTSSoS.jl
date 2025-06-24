811b3020221e3db452dba4337ba039d2c1dc026b
exaclior <yushengzhao2020@outlook.com>
2025-06-24 17:14:52 +0800

    fast fxi

--- test/heisenberg.jl
+++ test/heisenberg.jl
@@ -1,7 +1,10 @@
 using Test, NCTSSoS
+
 using MosekTools
 
-N = 10
+SOLVER = Mosek.Optimizer
+
+N = 6
 @ncpolyvar x[1:N] y[1:N] z[1:N]
 
 ham = sum(ComplexF64(1 / 4) * op[i] * op[mod1(i + 1, N)] for op in [x, y, z] for i in 1:N)
@@ -10,11 +13,13 @@ eq_cons = reduce(vcat, [[x[i] * y[i] - im * z[i], y[i] * x[i] + im * z[i], y[i] 
 
 pop = PolyOpt(ham; eq_constraints=eq_cons, comm_gps=[[x[i], y[i], z[i]] for i in 1:N], is_unipotent=true)
 
-solver_config = SolverConfig(optimizer=Mosek.Optimizer, mom_order = 2)
+solver_config = SolverConfig(optimizer=SOLVER, mom_order=1, cs_algo=MF(), ts_algo = MMD())
 
 # how do I deal with complex constraint?
 res = cs_nctssos(pop, solver_config)
-res.objective / N
+
+res = cs_nctssos_higher(pop,res,solver_config)
+@test res.objective / N ≈ -0.467129 atol=1e-6
 
 
 
