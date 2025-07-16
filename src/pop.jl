276bebfc69d08daf695ac9cac3dbae6e48b5c597
exaclior <yushengzhao2020@outlook.com>
2025-07-16 10:54:59 +0800

    impl ComplexPolyOpt

--- src/pop.jl
+++ src/pop.jl
@@ -32,6 +32,7 @@ struct PolyOpt{P} <: OptimizationProblem
     is_projective::Bool # X^2 = X. Is projective.
 end
 
+
 """
     polyopt(objective::P; eq_constraints=Any[], ineq_constraints=Any[], comm_gps=Vector{Variable}[], is_unipotent::Bool=false, is_projective::Bool=false) where {T,P<:AbstractPolynomial{T}}
 
@@ -68,7 +69,7 @@ function polyopt(objective::P; eq_constraints=Any[], ineq_constraints=Any[], com
     return PolyOpt{P}(objective, eq_cons, ineq_cons, vars, comm_gps, is_unipotent, is_projective)
 end
 
-function Base.show(io::IO, pop::PolyOpt)
+function Base.show(io::IO, pop::P) where {P<:OptimizationProblem}
     cons_str(cons::Vector{P}, iseq::Bool) where {P} =
         join(["$(string(c)) " * (iseq ? "= 0" : ">= 0 \n") for c in cons], " \t")
     res_str = """
@@ -85,4 +86,40 @@ function Base.show(io::IO, pop::PolyOpt)
             $(pop.is_projective) \n
     """
     print(io, res_str)
+end
+
+
+# ComplexPolyOpt is not only for coefficients being complex,
+# but the product of variables are not hermitians
+struct ComplexPolyOpt{P} <: OptimizationProblem
+    objective::P
+    eq_constraints::Vector{P} # NOTE: assuming constraints are all simplified using comm_gp, is_unipotent, and is_projective
+    ineq_constraints::Vector{P}
+    variables::Vector{Variable}
+    comm_gps::Vector{Vector{Variable}} # Vectors of Set of variables that commutes with variables not in the same set
+    is_unipotent::Bool # square to 1. Examples: Pauli Operators, SWAP Operators
+    is_projective::Bool # X^2 = X. Is projective.
+end
+
+
+function cpolyopt(objective::P; eq_constraints=Any[], ineq_constraints=Any[], comm_gps=Vector{Variable}[], is_unipotent::Bool=false, is_projective::Bool=false) where {T,P<:AbstractPolynomial{T}}
+    @assert !(T <: Integer) "The polynomial coefficients can not be integers (not supported by JuMP solvers)."
+    eq_cons = unique!(collect(P, eq_constraints))
+    ineq_cons = unique!(collect(P, ineq_constraints))
+    vars = sorted_union(variables(objective), variables.(eq_cons)..., variables.(ineq_cons)...)
+    if !isempty(comm_gps)
+        @assert all([isempty(intersect(gp_a, gp_b)) for gp_a in comm_gps, gp_b in comm_gps if gp_a != gp_b]) "The commutative groups must be disjoint."
+        @assert issubset(union(comm_gps...), vars) "The commutative variables must be a subset of the variables."
+    else
+        push!(comm_gps, vars)
+    end
+    @assert !(is_unipotent && is_projective) "The problem cannot be both unipotent and projective."
+
+    sa = SimplifyAlgorithm(comm_gps, false, false)
+    @assert FastPolynomials.is_symmetric(objective, sa) "Objective must be symmetric"
+    for ineq in ineq_constraints
+        @assert FastPolynomials.is_symmetric(ineq, sa) "Inequality constraints must be symmetric"
+    end
+
+    return ComplexPolyOpt{P}(objective, eq_cons, ineq_cons, vars, comm_gps, is_unipotent, is_projective)
 end
